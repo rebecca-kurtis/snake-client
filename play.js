9be8fad473d1b972b6977250ab3ca1c023a0d600
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2022-11-14 22:30:20 -0800

    Adding play.js and implementing snake code set up for idle

---
+++ play.js
@@ -0,0 +1,21 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',// IP address here,
+    port: 50541,// PORT number here,
+  });
+
+  conn.on('data', () => {
+    console.log('you ded cuz you idled');
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
